7b58e19e2af59b5889a349f99ebc51a30f9e0b64
ramil <ramil>
2024-09-05 08:04:28 +0000

    Replace usage of nocrypto for mirage-crypto, digestif, base64.

--- util.ml
+++ util.ml
@@ -2,30 +2,27 @@ let init_random =
   let initialized = ref false in
   fun () ->
     if not !initialized then
-      let () = Nocrypto_entropy_unix.initialize () in
+      let () =
+        Mirage_crypto_rng_lwt.initialize (module Mirage_crypto_rng.Fortuna) in
       initialized := true
 
-let random_bytes n =
-  Nocrypto.Rng.generate n |> Cstruct.to_bytes
-
 let random_string n =
-  Nocrypto.Rng.generate n |> Cstruct.to_string
+  Mirage_crypto_rng.generate n
+
+let random_bytes n = n |> random_string |> Bytes.unsafe_of_string
 
 module Base64 = struct
   type t = Base64 of string
 
   let create str = Base64 str
 
-  let of_string str =
-    Cstruct.of_string str
-    |> Nocrypto.Base64.encode
-    |> Cstruct.to_string
-    |> create
+  let of_string str = str |> Base64.encode_string |> create
 
   let as_string (Base64 str) = str
 end
 
 let sha1 str =
-  Cstruct.of_string str
-  |> Nocrypto.Hash.SHA1.digest
-  |> Cstruct.to_string
+  let module Hash = Digestif.SHA1 in
+  str
+  |> Hash.digest_string
+  |> Hash.to_raw_string
